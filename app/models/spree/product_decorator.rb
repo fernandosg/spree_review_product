37708047c3ac8a385da7a8c9cd1beef67fcc3a9c
fernandosegurabigo <fernandosegom@gmail.com>
2019-03-17 22:03:01 -0600

    Implementando libería starrr para mostrar el rating a añadir en la reseña

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,12 @@
 Spree::Product.class_eval do
   has_many :spree_reviews, class_name: 'Spree::Review'
+  attr_accessor :my_rating
+
+  def i_had_already_voting spree_user_id
+    return true unless @my_rating.nil?
+    review = Spree::Review.find_by_spree_product_id_and_spree_user_id(id, spree_user_id)
+    return nil if review.nil?
+    @my_rating = review.rating
+    @my_rating
+  end
 end
--- app/models/spree/review.rb
+++ app/models/spree/review.rb
@@ -1,4 +1,5 @@
 class Spree::Review < ApplicationRecord
   belongs_to :spree_product, foreign_key: 'spree_product_id', class_name: "Spree::Product"
   belongs_to :spree_user, foreign_key: 'spree_user_id', class_name: "Spree::User"
+
 end
